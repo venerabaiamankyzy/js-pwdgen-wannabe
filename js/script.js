88f8cfdbbc3d6829075a51b0dd1d2bf1ef162d1f
Venera Baiaman kyzy <venerabayaman@gmail.com>
2023-01-20 15:16:56 +0100

    done

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let username = prompt('Come il suo nome?');
+username = username.replace(username[0], username[0].toUpperCase());
+
+let surname = prompt('Come il suo cognome?'); 
+surname = surname.replace(surname[0], surname[0].toUpperCase());
+
+let usercolor = prompt('Qual è il suo colore preferito?');
+
+let userdate = `Il suo nome è ${username}.<br />  Il suo cognome è ${surname}.<br />  Il suo colore preferito è ${usercolor}`;
+
+document.getElementById('main-title').innerHTML = userdate;
